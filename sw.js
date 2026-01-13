b046ba0d396e0709b71cdf92d9ddc805623e3ab7
CarlosOtz <88871747+CarlosOtz@users.noreply.github.com>
2026-01-12 23:53:38 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "mis-gastos-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
+});
